1bac1332c199faf2afef11e18ef68e71487086ab
Shripal <shripal@gmail.com>
2023-06-03 13:11:41 +0530

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quod eligendi quidem, excepturi commodi aperiam incidunt molestiae obcaecati sunt veritatis libero nam, nostrum totam! In voluptatum facilis dolor numquam, molestias, nobis non ullam mollitia vero nam nulla, temporibus vel quod sint!
+      </p>
+    </section>
+  );
+};
+
+export default About;
